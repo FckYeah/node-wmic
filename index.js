19e002931f2b33a4b544a042f091b52ffdb00ae1
Viktor Pepe <8505287+FckYeah@users.noreply.github.com>
2025-05-13 18:45:59 +0200

    Set increased max buffer size

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { execFile, execFileSync } = require('child_process');
 const iconv = require('iconv-lite');
 const path = require('path');
 
+const MAX_BUFFER_SIZE = 100 * 1024 * 1024;
+
 const wmic = path.join(process.env.SystemRoot, 'System32', 'wbem', 'WMIC.exe');
 
-const aliasList = execFileSync(wmic, ['ALIAS', 'LIST', 'BRIEF']);
+const aliasList = execFileSync(wmic, ['ALIAS', 'LIST', 'BRIEF'], { maxBuffer: MAX_BUFFER_SIZE });
 const aliasLines = iconv.decode(aliasList, 'GB2312').split(/\r\n/);
 const aliasItems = aliasLines.slice(1, aliasLines.length - 2).map(line => line.split(/\s{2,}/)[0]);
 
@@ -13,7 +15,7 @@ const data = {};
 for (let aliasItem of aliasItems) {
   data[aliasItem] = () =>
     new Promise((resolve, reject) => {
-      execFile(wmic, [aliasItem, 'get', '/VALUE'], (err, stdout, stderr) => {
+      execFile(wmic, [aliasItem, 'get', '/VALUE'], { maxBuffer: MAX_BUFFER_SIZE }, (err, stdout, stderr) => {
         if (err || stderr) {
           reject(err || stderr);
         }
